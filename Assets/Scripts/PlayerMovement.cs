20f0217db863fe6a330b748f3d374602e3fe89ff
jakmos138 <jakub.moszynski138@gmail.com>
2023-11-12 16:58:49 +0100

    Initial Setup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float acceleration = 2f;
+    private bool isGrounded = true;
+    private bool isGrappling = false;
+    private int dashCount = 1;
+    private Rigidbody2D rb;
+    private Vector3 grappleTarget;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Dash();
+        } else if (Input.GetMouseButtonDown(1)) {
+            Grapple();
+        }
+    }
+
+    void Dash()
+    {
+        if (dashCount > 0)
+        {
+            // Dash logic
+        }
+    }
+
+    void Grapple()
+    {
+        if (isGrappling)
+        {
+            isGrappling = false;
+            // Stop Grapple
+        } else
+        {
+            isGrappling = true;
+            // Begin Grapple
+        }
     }
 }
